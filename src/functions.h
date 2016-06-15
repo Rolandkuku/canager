7e607b321fe52c9d2a56386473c6492e2596be80
Roland Kullet <renaud.bellec.3@gmail.com>
2016-06-15 16:23:53 +0200

    team file creation

--- src/functions.h
+++ src/functions.h
@@ -30,4 +30,16 @@ int readInput(char *str, int len) {
     }
 }
 
+void clean(const char *buffer, FILE *fp)
+{
+    char *p = strchr(buffer,'\n');
+    if (p != NULL)
+        *p = 0;
+    else
+    {
+        int c;
+        while ((c = fgetc(fp)) != '\n' && c != EOF);
+    }
+}
+
 #endif //CANAGER_FUNCTIONS_H
--- src/team.h
+++ src/team.h
@@ -5,6 +5,8 @@
 #ifndef CANAGER_TEAM_H
 #define CANAGER_TEAM_H
 
+#include <unistd.h>
+
 int makingTeam() {
     printf("\n\n\n-- Team Menu --\n");
     printf("1 - New Team\n");
@@ -16,6 +18,21 @@ int makingTeam() {
     if (strncmp(choice, "1", 1) == 0) {
         // Créer une team
         // TODO: Créer un nouveau fichier avec nommé d'après le nom de la team
+        char * cwd;
+        cwd = strcat(getcwd(0, 0), "/data/");
+        printf("Name of the team : \n");
+        char * file_name = malloc(sizeof(char)*50);
+        fgets(file_name, sizeof(file_name), stdin);
+        clean(file_name, stdin);
+        char * file_path = strcat(cwd, strcat(file_name, ".xml"));
+        FILE* team_file = NULL;
+        team_file = fopen(file_path, "w+");
+        if (team_file != NULL) {
+            printf("Created new file with the name : %s", file_name);
+        } else {
+            printf("Cannot open team file");
+        }
+        fclose(team_file);
     }
     else if (strncmp(choice, "2", 1) == 0) {
         // Créer un teammate
