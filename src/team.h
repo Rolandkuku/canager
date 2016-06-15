72ae1b17594dbb7a5666c67719dd6d4455712421
Roland Kullet <renaud.bellec.3@gmail.com>
2016-06-15 17:00:02 +0200

    quick fix

--- src/team.h
+++ src/team.h
@@ -5,6 +5,8 @@
 #ifndef CANAGER_TEAM_H
 #define CANAGER_TEAM_H
 
+int makeNewTeam();
+
 #include <unistd.h>
 #include <dirent.h>
 
@@ -19,39 +21,7 @@ int makingTeam() {
     if (strncmp(choice, "1", 1) == 0) {
         // Créer une team
         // TODO: Vérifier l'existence d'un fichier avant de créer
-        char * cwd;
-        cwd = strcat(getcwd(0, 0), "/data/");
-        printf("Name of the team : \n");
-        char * file_name = malloc(sizeof(char)*50);
-        fgets(file_name, sizeof(file_name), stdin);
-        clean(file_name, stdin);
-        file_name = strcat(file_name, ".xml");
-        // Look for other filenames
-        DIR * dp;
-        struct dirent * ep;
-        dp = opendir(cwd);
-        if (dp != NULL) {
-            while (ep =readdir(dp)) {
-                if (strcmp(file_name, ep->d_name) == 0) {
-                    printf("Team %s already exists !", file_name);
-                    return 1;
-                }
-            }
-            closedir(dp);
-        } else {
-            printf("Couldn't open directory");
-        }
-
-        // Creating new file
-        char * file_path = strcat(cwd, file_name);
-        FILE* team_file = NULL;
-        team_file = fopen(file_path, "w+");
-        if (team_file != NULL) {
-            printf("Created new file with the name : %s", file_name);
-        } else {
-            printf("Cannot open team file");
-        }
-        fclose(team_file);
+        makeNewTeam();
     }
     else if (strncmp(choice, "2", 1) == 0) {
         // Créer un teammate
@@ -62,6 +32,45 @@ int makingTeam() {
         // TODO: choisir parmis les fichiers team et afficher le contenu
     }
     else if (strncmp(choice, "4", 1) == 0) {
+        return 1;
+    }
+}
+
+int makeNewTeam() {
+    char * cwd;
+    cwd = strcat(getcwd(0, 0), "/data/");
+    printf("Name of the team : \n");
+    char * file_name = malloc(sizeof(char)*50);
+    fgets(file_name, sizeof(file_name), stdin);
+    clean(file_name, stdin);
+    file_name = strcat(file_name, ".xml");
+    // Look for other filenames
+    DIR * dp;
+    struct dirent * ep;
+    dp = opendir(cwd);
+    if (dp != NULL) {
+        while (ep =readdir(dp)) {
+            if (strcmp(file_name, ep->d_name) == 0) {
+                printf("Team %s already exists !", file_name);
+                return 1;
+            }
+        }
+        closedir(dp);
+    } else {
+        printf("Couldn't open directory");
+        return 1;
+    }
+
+    // Creating new file
+    char * file_path = strcat(cwd, file_name);
+    FILE* team_file = NULL;
+    team_file = fopen(file_path, "w+");
+    if (team_file != NULL) {
+        printf("Created new file with the name : %s", file_name);
+        fclose(team_file);
+        return 0;
+    } else {
+        printf("Cannot open team file");
         return 1;
     }
 }
